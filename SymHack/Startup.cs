20343b243af19c290138ac922ec01e138a1d8d0f
erinb <erinbradley27@hotmail.com>
2018-09-27 16:28:34 -0400

    Add project files.

---
+++ SymHack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SymHack.Startup))]
+namespace SymHack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
